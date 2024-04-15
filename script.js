2781dcfb84db2878fb4244cc1f227f99204d244f
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-04-15 12:46:02 +0200

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const increaseBtn = document.querySelector('#increase');
 const decreseBtn = document.querySelector('#decrease');
 const resetBtn = document.querySelector('#reset');
+const counterText = document.querySelector('#counter');
 
+let counter = 0;
+
+increaseBtn.addEventListener('click' , function() {
+  counter++;
+  counterText.textContent = counter;
+})
+
+decreseBtn.addEventListener('click' , function() {
+  counter--;
+  counterText.textContent = counter;
+})
+
+resetBtn.addEventListener('click' , function() {
+  counter = 0;
+  counterText.textContent = counter;
+})
 
